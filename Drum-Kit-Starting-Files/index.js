0849aba157798a8655787b32f77c7b65415c6af0
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-12-21 00:24:36 +0600

    click

--- Drum-Kit-Starting-Files/index.js
+++ Drum-Kit-Starting-Files/index.js
@@ -1,5 +1,29 @@
-document.querySelector(".w", wClicked);
+document.querySelector(".w").addEventListener("click", function() {
+    alert("w clicked");
+});
 
-function wClicked(){
-    alert("hi");
-}
+document.querySelector(".a").addEventListener("click", function() {
+    alert("a clicked");
+});
+
+document.querySelector(".s").addEventListener("click", function() {
+    alert("s clicked");
+});
+
+document.querySelector(".d").addEventListener("click", function() {
+    alert("d clicked");
+});
+
+document.querySelector(".j").addEventListener("click", function() {
+    alert("j clicked");
+});
+
+document.querySelector(".k").addEventListener("click", function() {
+    alert("k clicked");
+});
+
+document.querySelector(".l").addEventListener("click", function() {
+    alert("l clicked");
+});
+
+

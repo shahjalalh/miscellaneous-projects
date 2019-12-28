110fca75e948d625b3fff26721d83e60535acab4
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-12-28 14:38:14 +0600

    bmi calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -18,6 +18,19 @@ app.post("/", function(req, res){
     res.send("The result of the calculation is " + result);
 });
 
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res){
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight / Math.pow(height, 2);
+
+    res.send("Your BMI is " + bmi);
+});
+
 app.listen(3000, function(){
     console.log("Server is running port 3000.");
 });

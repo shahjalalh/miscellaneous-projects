a60344b8d38f6549bf0798e25553c4ea6be1aef8
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-12-30 17:58:59 +0600

    newsletter signup

---
+++ newsletter-signup/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/public', express.static("public"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+
+    first_name = req.body.first_name;
+    last_name = req.body.last_name;
+    email = req.body.email;
+
+    console.log(req.body);
+    console.log(first_name, last_name, email);
+
+    // https://mailchimp.com/developer/
+});
+
+app.listen(3000, function(){
+    console.log("server is running on port 3000");
+});

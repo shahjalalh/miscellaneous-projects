7d31648b6c8612a390894f1e7ebfa8ad74927f63
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-12-30 14:40:05 +0600

    bitcoin ticker

--- bitcoin-ticker/index.js
+++ bitcoin-ticker/index.js
@@ -12,21 +12,35 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
     
-    crypto = req.body.crypto;
-    fiat = req.body.fiat;
+    var crypto = req.body.crypto;
+    var fiat = req.body.fiat;
+
+    var amount = req.body.amount;
     
-    request("https://apiv2.bitcoinaverage.com/indices/global/ticker/"+crypto+fiat, function(error, response, body){
+    var options = {
+        url: "https://apiv2.bitcoinaverage.com/convert/global",
+        method: "GET",
+        qs: {
+            from: crypto,
+            to: fiat,
+            amount: amount,
+        }
+    };
+    
+    // https://www.npmjs.com/package/request#requestoptions-callback
+    // "https://apiv2.bitcoinaverage.com/indices/global/ticker/"+crypto+fiat
+    request(options, function(error, response, body){
         
         var data = JSON.parse(body);
-        var price = data.last;
+        var price = data.price;
 
-        var currentDate = data.display_timestamp;
+        var currentDate = data.time;
 
         res.write("<p>The current date is " + currentDate + "</p>");
 
-        res.write("<h1>The price of "+ crypto + " is " + price + " "+ fiat +"</h1>");
+        res.write("<h1>The "+ amount + crypto + " is currently worth" + price + " in "+ fiat +"</h1>");
 
-        res.send("");
+        //res.send("");
     });
 });
 

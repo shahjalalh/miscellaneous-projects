d943c2252e881050ebc71a0d12ef41b3008b8f05
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-12-31 10:30:51 +0600

    api integration

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -17,10 +17,43 @@ app.post("/", function(req, res){
     last_name = req.body.last_name;
     email = req.body.email;
 
+    var data = {
+        "members": [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: first_name,
+                    LNAME: last_name
+                }
+            }
+        ]
+    }
+
+    var jsonData = JSON.stringify(data);
+
     console.log(req.body);
     console.log(first_name, last_name, email);
 
     // https://mailchimp.com/developer/
+    // https://usX.api.mailchimp.com/3.0/list/7b80f67abe
+
+    options = {
+        url: "https://us18.api.mailchimp.com/3.0/list/mailchimp-list-id",
+        method: "POST",
+        headers: {
+            "Authorization": "shahjalal mailchimp-api-key-dataserver"
+        },
+        body: jsonData
+    }
+
+    request(options, function(error, response, body){
+        if(error){
+            console.log(error);
+        }else{
+            console.log(response.status);
+        }
+    });
 });
 
 app.listen(3000, function(){
@@ -28,5 +61,6 @@ app.listen(3000, function(){
 });
 
 
-// mailchimp-api-key
+// mailchimp-api-key-dataserver
+// https://status.mailchimp.com/ is for dataserver
 // mailchimp-list-id

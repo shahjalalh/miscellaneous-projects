a19c7c61afd6bb46f55cdac823b6040bdf6102f0
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-01-06 18:02:54 +0600

    todo list

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -5,8 +5,9 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-let items = [];
+let items = ["Need Food", "Buy Food", "Eat Food"];
 
 app.get('/', function (req, res) {
     let today = new Date();

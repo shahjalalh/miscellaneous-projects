f308e123b73704b8853212608f2eaa1e3c50d680
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-01-07 10:24:41 +0600

    todo list

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require( __dirname + "/date.js");
 
 const app = express();
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let items = ["Need Food", "Buy Food", "Eat Food"];
+let workItems = []
 
 app.get('/', function (req, res) {
+    /*
     let today = new Date();
-    
+
     let options = {
         weekday: "long",
         day: "numeric",
@@ -19,18 +22,42 @@ app.get('/', function (req, res) {
     }
 
     day = today.toLocaleDateString("en-US", options);
+    */
+
+    // let day = date.getDate();
+    let day = date.getDay();
 
     // res.render('list', {kindOfDay: weekdays[today.getDay()]});
-    res.render('list', { kindOfDay: day, newListItems: items });
+    res.render('list', { listTitle: day, newListItems: items });
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
     let item = req.body.newItem;
 
-    items.push(item);
+    if (req.body.list === "work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
 
-    res.redirect("/");
+        res.redirect("/");
+    }
 
+
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", function (req, res) {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
 });
 
 app.listen(3000, function () {
---
+++ todolist-v1/date.js
@@ -0,0 +1,25 @@
+module.exports.getDate = function(){
+    const today = new Date();
+
+    const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+    }
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    return day;
+}
+
+module.exports.getDay = function(){
+    const today = new Date();
+
+    const options = {
+    weekday: "long"
+    }
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    return day;
+}

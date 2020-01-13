ec5f53c4a724b68df73106d1142d65ee465fe6a0
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-01-13 16:42:07 +0600

    compose

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -19,12 +19,25 @@ app.get("/", function(req, res){
   res.render("home", {homeStartingContent: homeStartingContent});
 });
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
 
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
 
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
 
+app.post("/compose", function(req, res){
+  let title = req.body.postTitle;
+  let post = req.body.post
 
+  console.log(title, post);
 
-
+});
 
 
 

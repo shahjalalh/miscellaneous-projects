49fadeefe5933bc542c53d49abbdf5a51bc5c454
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-01-18 12:01:04 +0600

    fixed warning messages

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 // const items = ["Buy Food", "Cook Food", "Eat Food"];
 // const workItems = [];

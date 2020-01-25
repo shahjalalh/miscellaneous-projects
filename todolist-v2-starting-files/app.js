0a559a83bd2c01e5ca4e3c416624ddb5ea649597
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-01-25 11:45:30 +0600

    todo list v2

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -4,12 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 // const date = require(__dirname + "/date.js");
+const _ = require("lodash");
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
@@ -37,6 +38,13 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+}
+
+const List = mongoose.model("List", listSchema);
+
 /*
 Item.insertMany(defaultItems, function(error, docs){
   if(error){
@@ -47,40 +55,49 @@ Item.insertMany(defaultItems, function(error, docs){
   }
 });
 */
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
 
   //const day = date.getDate();
 
-  Item.find({}, function(error, foundItems){
+  Item.find({}, function (error, foundItems) {
 
-    if(foundItems.length === 0){
-      Item.insertMany(defaultItems, function(error, docs){
-        if(error){
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (error, docs) {
+        if (error) {
           console.log(error);
-        }else{
+        } else {
           console.log("Successfully saved default items to database");
-          
+
         }
       });
 
       res.redirect("/");
-    }else{
-      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
     }
   });
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
 
   const itemName = req.body.newItem;
+  const listName = req.body.list;
 
   const item = new Item({
     name: itemName,
   });
 
-  item.save();
+  if (listName === "Today") {
+    item.save();
 
-  res.redirect("/");
+    res.redirect("/");
+  } else {
+    List.findOne({ name: listName }, function (err, foundList) {
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
 
   /*
   if (req.body.list === "Work") {
@@ -93,34 +110,42 @@ app.post("/", function(req, res){
   */
 });
 
-app.post("/delete", function(req, res){
+app.post("/delete", function (req, res) {
   // console.log(req.body);
   // console.log(req.body.checkbox);
 
   const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
 
-  /*
-  Item.deleteOne({_id: checkedItemId}, function(error){
-    if(error){
-      console.log(error);
-    }else{
-      console.log("Successfully deleted one item!");
-      res.redirect("/");
-    }
-  });
-  */
-  // or,
-  // reference: https://mongoosejs.com/docs/api.html
-  Item.findByIdAndRemove(checkedItemId, function(error){
-    if(error){
-      console.log(error);
-    }else{
-      console.log("Successfully deleted one item!");
-      res.redirect("/");
-    }
-  });
+  if (listName === "Today") {
+    /*
+      Item.deleteOne({_id: checkedItemId}, function(error){
+        if(error){
+          console.log(error);
+        }else{
+          console.log("Successfully deleted one item!");
+          res.redirect("/");
+        }
+      });
+      */
+    // or,
+    // reference: https://mongoosejs.com/docs/api.html
+    Item.findByIdAndRemove(checkedItemId, function (error) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Successfully deleted one item!");
+        res.redirect("/");
+      }
+    });
+  }else{
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
+      if(!err){
+        res.redirect("/" + listName);
+      }
+    });
+  }
 
-  
 });
 
 /*
@@ -129,14 +154,36 @@ app.get("/work", function(req,res){
 });
 */
 
-app.get("/:location", function(req, res){
-  req.params.location;
+app.get("/:customListName", function (req, res) {
+  //console.log(req.params.customListName);
+
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.find({ name: customListName }, function (err, foundList) {
+
+    if (!err) {
+      if (!foundList) {
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+
+        list.save();
+
+        res.redirect("/" + customListName)
+      } else {
+        res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
+      }
+    }
+
+  });
+
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });

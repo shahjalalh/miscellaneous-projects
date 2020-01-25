20b7be07822fdabbb61d8e0a40a8ecf57ac0328c
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-01-25 14:00:11 +0600

    heroku

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -184,6 +184,12 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
+// heroku port configuration
+let port = process.env.PORT;
+if(port==null || port == ""){
+  port = 3000;
+}
+
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });

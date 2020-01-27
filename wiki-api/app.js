851c2416152dd04cff223c60ac611f6109d09e03
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-01-27 11:57:36 +0600

    server setup

--- wiki-api/app.js
+++ wiki-api/app.js
@@ -1,1 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express()
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/", function(req, res){
+    res.render('index');
+});
+
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});

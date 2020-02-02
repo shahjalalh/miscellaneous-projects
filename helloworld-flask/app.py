8f566bb913a0fd518b81cc23f5055e5f509539c9
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-02-02 12:35:17 +0600

    json format

--- helloworld-flask/app.py
+++ helloworld-flask/app.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return "Hello World!"
 
+@app.route('/bye')
+def bye_there():
+    data = {
+        'name': 'Elfarouk',
+        'age': 22,
+        'phones': [
+            {
+                'phoneName': 'iPhone8',
+                'phoneNumber': 23123323
+            },
+            {
+                'phoneName': 'Nokia',
+                'phoneNumber': 938928388
+            }
+        ]
+    }
+
+    return jsonify(data)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="127.0.0.1", port=80)

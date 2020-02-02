42066a9351f4242f9e865e52c11bb150208fe572
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-02-02 15:29:15 +0600

    python rest

--- helloworld-flask/app.py
+++ helloworld-flask/app.py
@@ -1,9 +1,29 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return "Hello World!"
+
+@app.route('/add_two_nums', method=["POST"])
+def add_two_nums:
+
+    # Get x, y from posted data
+    dataDict = request.get_json()
+
+    x = dataDict['x']
+    y = dataDict['y']
+
+    # Add z = x + y
+    z = x + y
+
+    # Prepare a JSON, 'z': z
+    retJSON = {
+        'z': z
+    }
+
+    # return jsonify(map_prepared)
+    return jsonify(retJSON), 200
 
 @app.route('/bye')
 def bye_there():

923da073963ea45464ca8c661479cf68ee24d12d
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-02-03 17:16:18 +0600

    express chaining

--- wiki-api/app.js
+++ wiki-api/app.js
@@ -18,6 +18,47 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
+app.route("/articles")
+    .get(function(req, res){
+        Article.find({}, function(err, foundArticles){
+            //console.log(foundArticles);
+            if(!err){
+                res.send(foundArticles);
+            }else{
+                res.send(err);
+            }
+            
+        });
+    })
+    .post(function(req, res){
+        title = req.body.title;
+        content = req.body.content;
+    
+        const newArticle = new Article({
+            title: title,
+            content: content
+        });
+        newArticle.save(function(err){
+            if(!err){
+                res.send("Successfully added a new article.");
+            }else{
+                res.send(err);
+            }
+        });
+    })
+    .delete(function(req, res){
+
+        Article.deleteMany({}, function(err){
+            if(!err){
+                res.send("Successfully deleted all articles.");
+            }else{
+                res.send(err);
+            }
+        });
+    
+    });
+
+/*
 app.get("/articles", function(req, res){
     Article.find({}, function(err, foundArticles){
         //console.log(foundArticles);
@@ -46,6 +87,19 @@ app.post("/articles", function(req, res){
         }
     });
 });
+
+app.delete('/articles', function(req, res){
+
+    Article.deleteMany({}, function(err){
+        if(!err){
+            res.send("Successfully deleted all articles.");
+        }else{
+            res.send(err);
+        }
+    });
+
+});
+*/
 
 app.get("/", function(req, res){
     res.render('index');

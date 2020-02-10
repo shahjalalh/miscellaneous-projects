f97296de60cf46391d576ebd98109f1d3029de60
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-02-10 10:10:06 +0600

    level-1 auth

--- secrets/app.js
+++ secrets/app.js
@@ -2,12 +2,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
     res.render("home");
@@ -21,6 +31,37 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("secrets");
+        }
+    });
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                if(foundUser.password == password){
+                    res.render("secrets");
+                }
+            }
+        }
+    });
+});
 
 
 app.listen(3000, function(){

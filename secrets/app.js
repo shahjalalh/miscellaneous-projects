14fb25a0f8e3c9c0ce4c1e50c2939eba6329886e
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-02-20 13:45:34 +0600

    secrets

--- secrets/app.js
+++ secrets/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -12,10 +13,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
 
 const User = new mongoose.model("User", userSchema);
 

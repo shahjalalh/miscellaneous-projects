1f5f0698f497941cd827e2592788ecc5c8e4d956
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-04-09 10:19:47 +0600

    youtube api

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class App extends React.Component{
     state = { videos: [], selectedVideo: null }
@@ -6,7 +7,7 @@ class App extends React.Component{
     render(){
         return (
             <div className="ui container">
-                <h1>videos browser...</h1>
+                <SearchBar />
             </div>
         );
     }

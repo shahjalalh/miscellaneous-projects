d087f18c23b7ad08bb9f9f25973fdf18d597730a
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-05-28 13:02:24 +0600

    streams initiated

---
+++ react/streams/client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component{
+
+    render(){
+        return (
+            <div className="ui container">
+                <h1>Streams App</h1>
+            </div>
+        );
+    }
+}
+
+export default App;

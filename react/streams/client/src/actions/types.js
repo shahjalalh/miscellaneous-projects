0582a9a0e6b19ae06b09bcef13892fade7c58be9
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-05-31 18:57:45 +0600

    Google oauth

---
+++ react/streams/client/src/actions/types.js
@@ -0,0 +1,2 @@
+export const SIGN_IN = 'SIGN_ID';
+export const SIGN_OUT = 'SIGN_OUT';
---
+++ react/streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import { signIn, signOut } from '../actions';
+
+class GoogleAuth extends React.Component{
+
+    componentDidMount(){
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client
+                .init({
+                    clientId: 'google-client-id',
+                    scope: 'email'
+                })
+                .then(() => {
+                    this.auth = window.gapi.auth2.getAuthInstance();
+
+                    console.log(this.auth);
+
+                    this.onAuthChange(this.auth.isSignedIn.get());
+                    this.auth.isSignedIn.listen(this.onAuthChange);
+                });
+        });
+    }
+
+    onAuthChange = isSignedIn => {
+        if(isSignedIn){
+            this.props.signIn(this.auth.currentUser.get().getId());
+        }else{
+            this.props.signOut();
+        }
+    };
+
+    onSignInClick = () => {
+        this.auth.signIn();
+    };
+
+    onSignOutClick = () => {
+        this.auth.signOut();
+    };
+
+    renderAuthButton(){
+        if(this.props.isSignedIn === null){
+            return null;
+        } else if(this.props.isSignedIn){
+            return (
+                <div onClick={this.onSignOutClick} className="ui red google button">
+                    <i className="google icon" />
+                    Sign Out
+                </div>
+            );
+        } else {
+            return (
+                <button onClick={this.onSignInClick} className="ui red google button">
+                    <i className="google icon" />
+                    Sign In with Google
+                </button>
+            );
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                {this.renderAuthButton()}
+            </div>
+        );
+    }
+}
+
+export default GoogleAuth;

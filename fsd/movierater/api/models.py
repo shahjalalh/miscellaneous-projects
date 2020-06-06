104319112856371630e737c6a21ca1cf9eb42a45
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-06-06 21:46:48 +0600

    django backend created

--- fsd/movierater/api/models.py
+++ fsd/movierater/api/models.py
@@ -8,6 +8,26 @@ class Movie(models.Model):
     title  = models.CharField(max_length=32)
     description = models.TextField(max_length=360)
 
+    def no_of_ratings(self):
+        ratings = Rating.objects.filter(movie=self)
+
+        return len(ratings)
+
+    def avg_rating(self):
+
+        sum = 0
+        ratings = Rating.objects.filter(movie=self)
+
+        for rating in ratings:
+            sum += rating.stars
+        
+        # import pdb;pdb.set_trace()
+        
+        if len(ratings) > 0:
+            return sum / len(ratings)
+        else:
+            return 0
+
 
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
@@ -15,5 +35,5 @@ class Rating(models.Model):
     stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
 
     class Meta:
-        unique_together = (('user', 'movie'),)
-        index_together = (('user', 'movie'),)
+        unique_together = [['user', 'movie']]
+        index_together = [['user', 'movie']]
--- fsd/movierater/api/urls.py
+++ fsd/movierater/api/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
 
+from .views import MovieViewSet, RatingViewSet, UserViewSet
+
 router = routers.DefaultRouter()
+router.register('movies', MovieViewSet)
+router.register('ratings', RatingViewSet)
+router.register('users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),

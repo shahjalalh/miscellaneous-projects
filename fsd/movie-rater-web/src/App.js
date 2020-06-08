659aafbfd694fc228a3d2bdd4e168d0f1069b52e
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-06-08 16:43:02 +0600

    django web

--- fsd/movie-rater-web/src/App.js
+++ fsd/movie-rater-web/src/App.js
@@ -1,18 +1,84 @@
 import React, { Component } from 'react';
 import './App.css';
+import MovieList from './components/MovieList';
+import MovieDetails from './components/MovieDetails';
+import MovieForm from './components/MovieForm';
+import { withCookies } from 'react-cookie';
+var FontAwesome = require('react-fontawesome');
 
 class App extends Component {
+
+  state = {
+    movies: [],
+    selectedMovie: null,
+    editedMovie: null,
+    token: this.props.cookies.get('mr-token')
+  }
+
+  componentDidMount(){
+    if(this.state.token){
+      fetch('http://127.0.0.1:8000/api/movies/', {
+        method: 'GET',
+        headers: {
+          'Authorization': `Token ${this.state.token}`
+        }
+      }).then(resp => resp.json())
+      .then(res => this.setState({movies: res}))
+      .catch(error => console.log(error))
+    }else{
+      window.location.href = '/'
+    }
+  }
+
+  loadMovie = movie => {
+    this.setState({selectedMovie: movie, editedMovie: null});
+  }
+
+  movieDeleted = selMovie => {
+    const movies = this.state.movies.filter(movie => movie.id !== selMovie.id);
+    this.setState({movies: movies, selectedMovie: null})
+  }
+
+  editClicked = selMovie => {
+    this.setState({editedMovie: selMovie})
+  }
+
+  newMovie = () => {
+    this.setState({editedMovie: {title: '', description: ''}});
+  }
+
+  cancelForm = () => {
+    this.setState({editedMovie: null});
+  }
+
+  addMovie = () => {
+    this.setState({movies: [...this.state.movies, movie]})
+  }
 
   render(){
     return (
       <div className="App">
         <h1>
+          <FontAwesome name="film" />
           <span>Movie Rater</span>
         </h1>
+        <div className="logout">
+          <MovieList movies={this.state.movies} movieClicked={this.loadMovie} token={this.setState.token} movieDeleted={TouchList.movieDeleted} editClicked={TouchLists.editClicked} newMovie={this.newMovie}/>
+          
+          <div>
+            {
+              !this.state.editedMovie ? 
+                <MovieDetails movie={this.state.selectedMovie} updateMovie={this.loadMovie} token={this.state.token} />
+                : <MovieForm movie={this.state.editedMovie} cancelForm={this.cancelForm} newMovie={this.addMovie} editedMovie={this.loadMovie} token={this.state.token} />
+            }
+          </div>
+
+        </div>
+
       </div>
     );
   }
   
 }
 
-export default App;
+export default withCookies(App);
---
+++ fsd/movie-rater-web/src/components/MovieList.js
@@ -0,0 +1,49 @@
+import React from 'react';
+var FontAwesome = require('react-fontawesome')
+
+function MovieList(props){
+
+    const movieClicked = movie => evt => {
+        props.movieClicked(movie);
+    }
+
+    const editClicked = movie => {
+        props.editClicked(movie);
+    }
+
+    const removeClicked = movie => {
+        fetch(`http://127.0.0.1:8000/api/movies/${movie.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${this.props.token}`
+            }
+        }).then(resp => props.movieDeleted(movie))
+        .catch(error => console.log(error))
+    };
+
+    const newMovie = () => {
+        props.newMovie()
+    };
+
+    return (
+        <div>
+            {
+                props.movies.map(movie => {
+                    return (
+                        <div key={movie.id} className="movie-item">
+                            <h3 onClick={movieClicked(movie)}>
+                                {movie.title}
+                            </h3>
+                            <FontAwesome name="edit" onClick={() => editClicked(movie)} />
+                            <FontAwesome name="trash" onClick={() => removeClicked(movie)} />
+                        </div>
+                    );
+                })
+            }
+            <button onClick={newMovie}>Add New</button>
+        </div>
+    );
+}
+
+export default MovieList;

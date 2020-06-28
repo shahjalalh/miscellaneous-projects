fe88275542ed64a47882e15b7054f182ad3aed6d
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-06-28 21:15:35 +0600

    flutter

---
+++ flutter/i_am_poor/android/app/src/main/java/com/example/flutterapp/flutterapp/iampoor/i_am_poor/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutterapp.flutterapp.iampoor.i_am_poor;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ flutter/mi_card_flutter/android/app/src/main/java/com/example/flutterapp/flutterapp/micardflutter/mi_card_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutterapp.flutterapp.micardflutter.mi_card_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}

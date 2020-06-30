d0155e23fd9c1bc857aea29ec60ab498bec7c228
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-06-30 19:28:55 +0600

    flutter

---
+++ flutter/dicee_flutter/android/app/src/main/java/com/example/flutterapp/flutterapp/diceeflutter/dicee_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutterapp.flutterapp.diceeflutter.dicee_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}

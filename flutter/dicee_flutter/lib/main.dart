64a5900ea66b452cd46cea0b1041e0f6cfe3952c
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-07-03 10:54:31 +0600

    flutter

--- flutter/dicee_flutter/lib/main.dart
+++ flutter/dicee_flutter/lib/main.dart
@@ -15,21 +15,35 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 2;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = 6;
+                  print("Left button got pressed: $leftDiceNumber");
+                });
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print("Right button got pressed");
+              },
               child: Image.asset('images/dice2.png'),
             ),
           ),

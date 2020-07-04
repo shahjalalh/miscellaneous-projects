5d382fc8bff420ad52bd7ba0e9bc0817ba2e65ae
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-07-04 13:19:30 +0600

    flutter

--- flutter/xylophone_flutter/lib/main.dart
+++ flutter/xylophone_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() {
   runApp(XylophoneApp());
@@ -10,7 +11,80 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note1.wav');
+                  },
+                  child: Text(' '),
+                  color: Colors.red,
+                ),
+              ),
+              Container(
+                child: FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note2.wav');
+                  },
+                  child: Text(' '),
+                  color: Colors.orange,
+                ),
+              ),
+              Container(
+                child: FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note3.wav');
+                  },
+                  child: Text(' '),
+                  color: Colors.yellow,
+                ),
+              ),
+              Container(
+                child: FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note4.wav');
+                  },
+                  child: Text(' '),
+                  color: Colors.green,
+                ),
+              ),
+              Container(
+                child: FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note5.wav');
+                  },
+                  child: Text(' '),
+                  color: Colors.green.shade900,
+                ),
+              ),
+              Container(
+                child: FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note6.wav');
+                  },
+                  child: Text(' '),
+                  color: Colors.blue,
+                ),
+              ),
+              Container(
+                child: FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note7.wav');
+                  },
+                  child: Text(' '),
+                  color: Colors.deepPurple,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

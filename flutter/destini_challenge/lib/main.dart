1ef343ad290afb9cbe0ed76628bb03267988c600
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-07-11 07:32:35 +0600

    flutter

--- flutter/destini_challenge/lib/main.dart
+++ flutter/destini_challenge/lib/main.dart
@@ -27,6 +27,12 @@ class _StoryPageState extends State<StoryPage> {
     return Scaffold(
       body: Container(
         //TODO: Step 1 - Add background.png to this Container as a background image.
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
         padding: EdgeInsets.symmetric(vertical: 50, horizontal: 15),
         constraints: BoxConstraints.expand(),
         child: SafeArea(

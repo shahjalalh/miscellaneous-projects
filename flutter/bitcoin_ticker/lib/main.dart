c909c121ffe5b7478a81e4fa790648655ce6c9a7
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2020-07-16 13:15:25 +0600

    flutter

--- flutter/bitcoin_ticker/lib/main.dart
+++ flutter/bitcoin_ticker/lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'price_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.lightBlue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: PriceScreen(),
+    );
   }
 }
---
+++ flutter/flash_chat_flutter/android/app/src/main/java/com/example/flutterapp/flashchat/flutter/flash_chat_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutterapp.flashchat.flutter.flash_chat_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
